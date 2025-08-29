7dd6a3c970129b0418a2dea9de46623b945c5b73
rainer <rainer.collet@gmail.com>
2025-08-29 12:22:29 +0200

    .

--- src/test/resources/test_include_org.s
+++
@@ -1,6 +0,0 @@
-.ORG 0|0
-NOP          # Position [0,0]
-NOP          # Position [1,0]
-.ORG 0|1     # Setze Position auf [0,1]
-.INCLUDE "test_include_org_inc.s"
-NOP          # Position [0,2] (sollte sein)
--- src/test/resources/test_include_org_inc.s
+++
@@ -1,4 +0,0 @@
-.ORG 0|1     # Sollte relativ zu [0,1] sein, also [0,2]
-NOP          # Position [0,2] (sollte sein)
-.ORG 0|2     # Sollte relativ zu [0,1] sein, also [0,3]
-NOP          # Position [0,3] (sollte sein)
