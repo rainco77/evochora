1815233b0e566dd69a85173db29659239d06c6d7
rainer <rainer.collet@gmail.com>
2025-09-25 00:37:02 +0200

    .

--- src/main/java/org/evochora/datapipeline/api/resources/IContextualResource.java
+++ src/main/java/org/evochora/datapipeline/api/resources/IContextualResource.java
@@ -4,7 +4,7 @@ package org.evochora.datapipeline.api.resources;
  * An interface for resources that can provide specialized implementations based on their usage context.
  * <p>
  * When a resource implements this interface, the ServiceManager will call
- * {@link #getInjectedObject(ResourceContext)} to get the object that should be
+ * {@link #getWrappedResource(ResourceContext)} to get the object that should be
  * injected into the service, rather than injecting the resource itself. This allows
  * a single resource definition to provide different functionalities based on which
  * service and port it is connected to.
